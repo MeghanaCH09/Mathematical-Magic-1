53dc71865fac7be84fb5cccc1a17f7e855a078cf
MeghanaCH09 <meghanacheruvu@gmial.com>
2024-12-29 12:59:40 +0200

    Factors

---
+++ Factors of a Number.py
@@ -0,0 +1,8 @@
+def fact(number):
+    print(f"The factors of {number} are: ")
+    for i  in range(1,number+1):
+        if number % i==0:
+            print(i)
+
+number=int(input("Enter a number to find the factors: "))
+fact(number)
